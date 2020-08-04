8cbeb1252894a66e2e33124f23659d69ce41a828
UglanovEA <uglanovea@gmail.com>
2020-08-04 15:42:46 +0300

    ver 2.0

--- main.js
+++ main.js
@@ -8,9 +8,83 @@ $(document).ready(function () {
     $("body").removeClass("lock");
   });
 
-  $(".owl-carousel").owlCarousel({
+  const clubcontrols = $("#ClubInfoCarousel");
+  clubcontrols.owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    smartSpeed: 1000,
+  });
+  $("#clubcontrolsleft,#comcontrolsleft").click(function () {
+    clubcontrols.trigger("next.owl.carousel");
+  });
+  $("#clubcontrolsrigth, #comcontrolsright").click(function () {
+    clubcontrols.trigger("prev.owl.carousel");
+  });
+
+  const ourcontrols = $("#OurRidersCarousel");
+  ourcontrols.owlCarousel({
     items: 3,
     loop: true,
     dots: false,
+
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 3,
+      },
+    },
+  });
+
+  const commentcontrols = $("#CommentsCarousel");
+  commentcontrols.owlCarousel({
+    items: 1,
+    loop: true,
+    dots: false,
+    margin: 20,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+  $("#comcontrolsleft").click(function () {
+    commentcontrols.trigger("next.owl.carousel");
+  });
+  $("#comcontrolsright").click(function () {
+    commentcontrols.trigger("prev.owl.carousel");
+  });
+
+  const shopcontrols = $("#ShopCarousel");
+  shopcontrols.owlCarousel({
+    items: 3,
+    loop: true,
+    dots: false,
+    margin: 20,
+    smartSpeed: 1000,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      1000: {
+        items: 2,
+      },
+    },
+  });
+  $("#comcontrolsleft").click(function () {
+    commentcontrols.trigger("next.owl.carousel");
+  });
+  $("#comcontrolsright").click(function () {
+    commentcontrols.trigger("prev.owl.carousel");
   });
 });
