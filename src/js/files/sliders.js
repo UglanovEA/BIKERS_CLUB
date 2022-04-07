139231f252ad376e34df708c85731e59b2dc847f
UglanovEA <uglanovea@gmail.com>
2022-04-07 15:59:25 +0300

    ver 4.1

--- src/js/files/sliders.js
+++ src/js/files/sliders.js
@@ -7,7 +7,7 @@
 // Подключаем слайдер Swiper из node_modules
 // При необходимости подключаем дополнительные модули слайдера, указывая их в {} через запятую
 // Пример: { Navigation, Autoplay }
-import Swiper, { Navigation, EffectCreative } from 'swiper';
+import Swiper, { Navigation, EffectCreative, Autoplay } from 'swiper';
 /*
 Основниые модули слайдера:
 Navigation, Pagination, Autoplay,
@@ -27,10 +27,10 @@ import '../../scss/base/swiper.scss';
 function initSliders() {
   // Перечень слайдеров
   // Проверяем, есть ли слайдер на стронице
-  if (document.querySelector('.page-slider__slider-one__one')) {
+  if (document.querySelector('.page-slider__slider')) {
     // Указываем скласс нужного слайдера
     // Создаем слайдер
-    new Swiper('.page-slider__slider-one__one', {
+    new Swiper('.page-slider__slider', {
       // Указываем скласс нужного слайдера
       // Подключаем модули слайдера
       // для конкретного случая
@@ -115,45 +115,32 @@ function initSliders() {
       on: {},
     });
   }
-  if (document.querySelector('.page-slider__slider-two__two')) {
+  if (document.querySelector('.page-testmoinals__slider')) {
     // Указываем скласс нужного слайдера
     // Создаем слайдер
-    new Swiper('.page-slider__slider-two__two', {
+    new Swiper('.page-testmoinals__slider', {
       // Указываем скласс нужного слайдера
       // Подключаем модули слайдера
       // для конкретного случая
-      modules: [Navigation, EffectCreative],
-      effect: 'creative',
-      creativeEffect: {
-        prev: {
-          shadow: true,
-          translate: [0, 0, -400],
-        },
-        next: {
-          translate: ['100%', 0, 0],
-        },
-      },
+      modules: [Navigation, Autoplay],
       observer: true,
       observeParents: true,
-      slidesPerView: 1,
-      // spaceBetween: 0,
+      slidesPerView: 2,
+      spaceBetween: 80,
       // autoHeight: true,
       speed: 800,
-
       //touchRatio: 0,
       //simulateTouch: false,
-      //loop: true,
+      loop: true,
       //preloadImages: false,
       //lazy: true,
 
-      /*
-		  // Эффекты
-		  effect: 'fade',
-		  autoplay: {
-			  delay: 3000,
-			  disableOnInteraction: false,
-		  },
-		  */
+      // Эффекты
+      effect: 'fade',
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
 
       // Пагинация
       /*
@@ -170,35 +157,25 @@ function initSliders() {
 			  draggable: true,
 		  },
 		  */
-
       // Кнопки "влево/вправо"
       navigation: {
-        prevEl: '.swiper-button-prev',
-        nextEl: '.swiper-button-next',
+        prevEl: '.page-testmoinals__arrow-prev',
+        nextEl: '.page-testmoinals__arrow-next',
+      },
+      // Брейкпоинты
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1268: {
+          slidesPerView: 2,
+        },
       },
 
-      // Брейкпоинты
-      /*
-		  breakpoints: {
-			  320: {
-				  slidesPerView: 1,
-				  spaceBetween: 0,
-				  autoHeight: true,
-			  },
-			  768: {
-				  slidesPerView: 2,
-				  spaceBetween: 20,
-			  },
-			  992: {
-				  slidesPerView: 3,
-				  spaceBetween: 20,
-			  },
-			  1268: {
-				  slidesPerView: 4,
-				  spaceBetween: 30,
-			  },
-		  },
-		  */
       // События
       on: {},
     });
